3a2bad40daf1ba8203d5c0661a664921a57e60aa
rahman-sohan <Sohanur@Scalebridger.com>
2025-06-01 12:39:05 +0600

    refactor: simplify AppController constructor and update global prefix in main.ts

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
 
     app.startAllMicroservices();
     app.enableCors({ origin: '*', methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', credentials: true });
-    app.setGlobalPrefix('api/v1/product');
+    app.setGlobalPrefix('api/v1');
 
     const port = process.env.PORT ?? 3001;
     await app.listen(port);
