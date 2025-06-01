a686b55f05a28f58868f221d2c2fb8c6bbee01ce
rahman-sohan <Sohanur@Scalebridger.com>
2025-06-01 12:00:43 +0600

    feat: implement health check endpoint and restructure application modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { ProductModule } from './product/product.module';
+import { DatabaseModule } from './database/database.module';
+import { MessageBrokerRabbitmqModule } from './message-broker-rabbitmq/message-broker-rabbitmq.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [DatabaseModule, MessageBrokerRabbitmqModule, ProductModule],
+    controllers: [AppController],
+    providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './common/errors/allException.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {
+        logger: ['error', 'warn', 'log', 'debug'],
+    });
 
-  app.enableCors({ origin: '*', methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', credentials: true });
-  app.setGlobalPrefix('api/v1/product');
+    app.useGlobalFilters(new AllExceptionsFilter());
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+            whitelist: true,
+            forbidNonWhitelisted: true,
+        }),
+    );
 
-  await app.listen(process.env.PORT ?? 3000);
-  console.log(`''''''''''Product Service is running on port ${process.env.PORT ?? 3001}'''''''''`);
+    app.startAllMicroservices();
+    app.enableCors({ origin: '*', methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', credentials: true });
+    app.setGlobalPrefix('api/v1/product');
+
+    const port = process.env.PORT ?? 3001;
+    await app.listen(port);
+    console.log(`''''''''''Product Service is running on port ${port}'''''''''`);
 }
 
 bootstrap();
